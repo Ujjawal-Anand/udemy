5ce1d5032dd4695703b75f63036a473870a3f3ab
andy1729 <ujjawalanand1729@gmail.com>
2020-02-04 20:50:37 +0530

    Updates

---
+++ precache-manifest.84cf27294ae8de533535819e898b6675.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0662fac4361e61689d14cf578f5680",
+    "url": "/udemy/index.html"
+  },
+  {
+    "revision": "8d91cc21a28a806d430a",
+    "url": "/udemy/static/js/2.8765e022.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/udemy/static/js/2.8765e022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "846f293f25c77d09a4a0",
+    "url": "/udemy/static/js/main.2ccaab7e.chunk.js"
+  },
+  {
+    "revision": "2f18cd6ca014f71ac470",
+    "url": "/udemy/static/js/runtime-main.ea1cc6bf.js"
+  }
+]);
