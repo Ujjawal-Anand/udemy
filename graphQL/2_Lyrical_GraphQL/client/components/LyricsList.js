78010c2a065084a15ec6e4098ef29c655f464459
andy1729 <ujjawalanand1729@gmail.com>
2020-02-07 09:27:23 +0530

    completed graphql-lyrical project

--- graphQL/2_Lyrical_GraphQL/client/components/LyricsList.js
+++ graphQL/2_Lyrical_GraphQL/client/components/LyricsList.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 
 class LyricsList extends Component {
+
+    likeLyric(id, likes) {
+        this.props.mutate({
+            variables: { id },
+            optimisticResponse: {
+                __typename: 'Mutation',
+                likeLyric: {
+                    id: id,
+                    __typename: "LyricType",
+                    likes: likes+1
+            }
+        }
+        })
+    }
     
     renderLyricsList() {
-        return this.props.lyrics.map(({id, content}) => {
+        return this.props.lyrics.map(({id, content, likes}) => {
             return (
             <li key={id} className="collection-item">
                 {content}
+
+            <div className="vote-box">
+                <i className="material-icons" onClick={()=> this.likeLyric(id, likes) }>thumb_up</i>
+                {likes}
+            </div>
             </li>
             );
         });
@@ -16,7 +37,6 @@ class LyricsList extends Component {
     render() {
         return (
             <div>
-                
                 <ul className="collection">
                     {this.renderLyricsList()}
                 </ul>
@@ -25,4 +45,11 @@ class LyricsList extends Component {
     }
 }
 
-export default LyricsList;
+const mutation = gql`mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }`;
+
+export default graphql(mutation)(LyricsList);
