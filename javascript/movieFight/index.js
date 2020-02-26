c42ed2622fa64356331d0f4c5ccb5b13be493fd9
andy1729 <ujjawalanand1729@gmail.com>
2020-02-26 10:05:53 +0530

    added debounce to input field

--- javascript/movieFight/index.js
+++ javascript/movieFight/index.js
@@ -1,1 +1,28 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '69d87029',
+            s: searchTerm
+        }
+    });
+    console.log(response.data)
+}
+
+const debounce = (func, delay=1000) => {
+    let timeoutId;
+    return (...args) => {
+        if(timeoutId) {
+            clearTimeout(timeoutId);
+        }
+       timeoutId = setTimeout(() => {
+           func.apply(null, args) 
+        }, delay);
+    };
+}
+
+const input = document.querySelector('#search_input_1');
+
+const onInput = (event) => {
+    fetchData(event.target.value);
+}
+input.addEventListener('input', debounce(onInput, 1000));
