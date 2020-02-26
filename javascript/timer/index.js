f3d16f54712c22f21b1dc4379d58adf6c839c110
andy1729 <ujjawalanand1729@gmail.com>
2020-02-26 09:18:09 +0530

    fixed bugs and commented the code

--- javascript/timer/index.js
+++ javascript/timer/index.js
@@ -2,15 +2,17 @@ const durationInput = document.querySelector('#duration');
 const startBtn = document.querySelector('#start');
 const pauseBtn = document.querySelector('#pause');
 const circle = document.querySelector('circle')
-let duration;
+let duration = 0;
 let currentOffset = 0;
+let pausedTime = 0;
+
 
 const perimeter = circle.getAttribute('r')*Math.PI*2;
 circle.setAttribute('stroke-dasharray', perimeter)
 
 var timer = new Timer(durationInput, startBtn, pauseBtn, {
     onStart(totalDuration) {
-        duration = totalDuration
+        duration = totalDuration;
     },
     onTick(remainingTime) {
         currentOffset = perimeter * remainingTime/duration - perimeter; //formula to calculate offset
@@ -18,5 +20,8 @@ var timer = new Timer(durationInput, startBtn, pauseBtn, {
     },
     onComplete() {
         console.log('Completed');
+    },
+    onPause() {
+        // on pause
     }
 });
--- javascript/timer/timer.js
+++ javascript/timer/timer.js
@@ -4,32 +4,36 @@ class Timer {
         this.startBtn = startBtn;
         this.pauseBtn = pauseBtn;
 
-        this.totalDuration = this.timeRemaining;
-
-        if(callBacks) {
+        // callbacks: ways of sending data from child to parent function, 
+        // first check whether callback parameter is passed or not
+        if(callBacks) {  
             this.onStart = callBacks.onStart;
             this.onTick = callBacks.onTick;
             this.onComplete = callBacks.onComplete;
+            this.onPause= callBacks.onPause;
         }
 
-        this.startBtn.addEventListener('click', this.start);
+        this.startBtn.addEventListener('click', this.start);  // added event listeners
         this.pauseBtn.addEventListener('click', this.pause);
     }
 
     start = () => {
+        // checks onStart callback function is passed or not
         if(this.onStart) {
-            this.onStart(this.totalDuration);
+            this.onStart(this.timeRemaining);
         }
         this.tick();
-        this.interval = setInterval(this.tick, 20);
-        this.startBtn.disabled = true;
+        this.interval = setInterval(this.tick, 20); 
+        // get instance of setInterval in global scope, so that it can be passed to clear the same
+        this.startBtn.disabled = true; // disables the start button, once the timer is started
     }
 
     tick = () => {
         // const timeDuration = this.timeRemaining;
         // this.timeRemaining = timeDuration - 1;
+
         if(this.timeRemaining <= 0) {
-            this.pause();
+            this.pause(); //  clear the time interval when it reaches to 0 
             if(this.onComplete) {
                 this.onComplete();
             }
@@ -46,13 +50,17 @@ class Timer {
     pause = () => {
         clearInterval(this.interval);
         this.startBtn.disabled = false;
-
+        if(this.onPause && this.timeRemaining > 0) {
+            this.onPause();
+        }
     }
 
+    // getter function
     get timeRemaining() {
-        return parseFloat(this.durationInput.value);
+        return parseFloat(this.durationInput.value); //value from durationInput is a string, convert it to float
     }
 
+    // setter function
     set timeRemaining(time) {
         this.durationInput.value = time.toFixed(2);
     }
