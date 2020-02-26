4906c20422816a1aa0255f4e79b4859ba430b4e6
andy1729 <ujjawalanand1729@gmail.com>
2020-02-26 13:35:35 +0530

    added dropdown list

--- javascript/movieFight/index.js
+++ javascript/movieFight/index.js
@@ -1,3 +1,18 @@
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label for="search_movie">Search Movie You Are Looking For</label>
+<input class="input" type="text" name="search_movie" />
+<div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results"></div>
+    </div>
+</div>
+`
+
+const input = document.querySelector('.input');
+const dropdown = document.querySelector('.dropdown');
+const resultWrapper = document.querySelector('.results');
+
 const fetchData = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
@@ -5,23 +20,28 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
         }
     });
+    if(response.data.Error) {
+        return [];
+    }
     return response.data.Search;
 }
 
-
-const input = document.querySelector('#search_input_1');
-
 const onInput = async (event) => {
     const movies = await(fetchData(event.target.value));
+    resultWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
 
     for (let movie of movies) {
-        const div = document.createElement('div');
+        const option = document.createElement('a');
+        const posterSrc = movie.Poster === "NA" ? '' : movie.Poster; 
 
-        div.innerHTML = `
-            <img src='${movie.Poster}' />
-            <h2>${movie.Title}</h2>
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+            <img src='${posterSrc}' />
+            ${movie.Title}
         `;
-        document.querySelector('#target').appendChild(div);
+        resultWrapper.appendChild(option);
     }
 }
+
 input.addEventListener('input', debounce(onInput, 1000));
